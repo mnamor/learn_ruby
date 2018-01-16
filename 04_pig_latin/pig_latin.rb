24ec904ae1ab1b14e471f5b22d10dac19f12d370
Roman <malaplateroman@gmail.com>
2018-01-16 16:45:48 +0100

    exo 04 piglatin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,27 @@
 #write your code here
+#define  variable words
+def vowel(letter)
+    vowel.tr['vowel , apple']
+end
+
+#define variable translate
+def translate(phrase)
+   words = phrase.split()                                 # Split sentence into array of words
+   words.each do |word|                                   #define a loop
+       if word.start_with?('a','e','i','o','u')           #define a condition
+           word << "ay"
+       else                                               #define a condition
+           while not word.start_with?('a','e','i','o','u')
+               if word.start_with?('qu')                  # Special case for qu combo
+                   word.insert(-1, 'qu')
+                   word[0..1] = ''
+               else
+                   word.insert(-1, word[0])               # Moves first letter to end of word
+                   word[0] = ''                           # Deletes first letter
+               end
+           end
+           word << "ay"                                    # if word begin by "ay"
+       end
+   end
+   words.join(" ")                                          #join resulte of words
+end
