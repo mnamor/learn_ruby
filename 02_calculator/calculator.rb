3d549ebde931274488c7796d511454fa3bc3bd91
Roman <malaplateroman@gmail.com>
2018-01-16 13:43:28 +0100

    exo 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add (num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum (arr)
+  somme = 0
+  arr.each do |x|
+    somme += x
+  end
+  return somme
+end
